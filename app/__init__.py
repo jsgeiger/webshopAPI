003240d19d9dbb768b959b3a14fe06c83ec95526
Julia Geiger <juliageiger94@web.de>
2020-02-21 21:00:46 +0100

    adds setup for database

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
-
+from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:rootroot@localhost:3306/WebshopAPI'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-from app import routes
+from app.route import routes
+db = SQLAlchemy()
+db.init_app(app)
 
 app.run(debug=True)
