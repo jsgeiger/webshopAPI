014776ecb3a2b33f2f8cc49232e5a500d6207a8f
Julia  Geiger <juliageiger94@web.de>
2020-03-07 21:29:21 +0100

    feature: add new book to database

--- app/model/author.py
+++ app/model/author.py
@@ -4,7 +4,6 @@ class AuthorModel(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     forename = db.Column(db.String(255),  nullable=False)
     surname = db.Column(db.String(255),  nullable=False)
-    books = db.relationship('BookModel', backref='author')
 
     def save_to_db(self):
         db.session.add(self)
--- app/model/book.py
+++ app/model/book.py
@@ -5,7 +5,8 @@ class BookModel(db.Model):
      title = db.Column(db.String(255),  nullable=False)
      description = db.Column(db.String(255),  nullable=False)
      image = db.Column(db.String(255))
-     author_id = db.Column(db.Integer, db.ForeignKey('author.id'), nullable=False)
+     author_id = db.Column(db.Integer, db.ForeignKey('author_model.id'), nullable=False)
+     author = db.relationship("AuthorModel", backref="books", primaryjoin="AuthorModel.id == BookModel.author_id")
 
      def save_to_db(self):
         db.session.add(self)
