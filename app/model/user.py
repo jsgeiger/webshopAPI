3d2718f4a40c802199b7d887fde19f7538b71a47
Julia Geiger <juliageiger94@web.de>
2020-03-29 14:05:12 +0200

    adds user model

---
+++ app/model/user.py
@@ -0,0 +1,16 @@
+from flask_user import UserMixin
+
+class UserModel(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    active = db.Column('is_active', db.Boolean(), nullable=False, server_dedault='1')
+    username = db.Column(db.String(100, collation='NOCASE'), nullable=False, unique=True)
+    password = db.Column(db.String(255), nullable=False, server_default='')
+    forename = db.Column(db.String(100, collation='NOCASE'), nullable=False, server_default='')
+    surname = db.Column(db.String(100, collation='NOCASE'), nullable=False, server_default='')
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return '<User %r>' % self.username
