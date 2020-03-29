9f23eadcef8879687d97bcc37c2fa31d649eb8b9
Julia Geiger <juliageiger94@web.de>
2020-03-29 20:04:17 +0200

    fixes typos in user.py

--- app/model/user.py
+++ app/model/user.py
@@ -1,12 +1,15 @@
+from app import db
+
 from flask_user import UserMixin
+
 
 class UserModel(db.Model, UserMixin):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    active = db.Column('is_active', db.Boolean(), nullable=False, server_dedault='1')
-    username = db.Column(db.String(100, collation='NOCASE'), nullable=False, unique=True)
+    active = db.Column('is_active', db.Boolean(), nullable=False, server_default='1')
+    username = db.Column(db.String(100), nullable=False, unique=True)
     password = db.Column(db.String(255), nullable=False, server_default='')
-    forename = db.Column(db.String(100, collation='NOCASE'), nullable=False, server_default='')
-    surname = db.Column(db.String(100, collation='NOCASE'), nullable=False, server_default='')
+    forename = db.Column(db.String(100), nullable=False, server_default='')
+    surname = db.Column(db.String(100), nullable=False, server_default='')
 
     def save_to_db(self):
         db.session.add(self)
