cbd41178f73202ca6e1d84cd1ac376f48460d237
Julia Geiger <juliageiger94@web.de>
2020-04-04 16:10:45 +0200

    adds save_to_db function to role model

--- app/model/Role.py
+++ app/model/Role.py
@@ -5,3 +5,10 @@ class Role(db.Model):
     __tablename__ = 'roles'
     id = db.Column(db.Integer(), primary_key=True)
     name = db.Column(db.String(50), unique=True)
+
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
+    def __repr__(self):
+        return '<Role %r>' % self.name
