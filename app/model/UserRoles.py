03b01644487f5d0cf3268b58953f2e9dc61cf5d6
Julia Geiger <juliageiger94@web.de>
2020-04-04 16:11:24 +0200

    fixes typos in UserRoles model

--- app/model/UserRoles.py
+++ app/model/UserRoles.py
@@ -4,5 +4,8 @@ from app import db
 class UserRoles(db.Model):
     __tablename__ = 'user_roles'
     id = db.Column(db.Integer(), primary_key=True)
-    user_id = db.Column(db.Integer(), db.ForeignKey('users.id', ondelete='CASCADE'))
+    user_id = db.Column(db.Integer(), db.ForeignKey('user_model.id', ondelete='CASCADE'))
     role_id = db.Column(db.Integer(), db.ForeignKey('roles.id', ondelete='CASCADE'))
+
+    def __repr__(self):
+        return '<UserRoles %r>' % self.name
