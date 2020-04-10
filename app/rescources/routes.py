74b55b98a6757ffc0c25403f121d45491cd5b397
Julia Geiger <juliageiger94@web.de>
2020-04-10 14:02:18 +0200

    adds user routes

--- app/rescources/routes.py
+++ app/rescources/routes.py
@@ -3,7 +3,8 @@ from flask import Flask, request, after_this_request, jsonify, request, make_res
 from flask_restful import Resource, Api, abort, reqparse
 from app.model.Author import AuthorModel
 from app.model.Book import BookModel
-from app.model.Role import Role
+from app.model.User import UserModel
+from app.model import Role
 
 
 class Books(Resource):
@@ -159,3 +160,62 @@ class Roles(Resource):
 
 
 api.add_resource(Roles, '/api/roles')
+
+
+class Users(Resource):
+    def get(self):
+        users = UserModel.query.all()
+
+        def to_json(x):
+            return {
+                'id': x.id,
+                'username': x.username,
+                'forename': x.foranme,
+                'surname': x.surname,
+                'roles': x.roles,
+            }
+
+        return {'Users': list(map(lambda x: to_json(x), users))}
+
+
+api.add_resource(Users, '/api/users')
+
+User_parser = reqparse.RequestParser()
+User_parser.add_argument('username')
+User_parser.add_argument('forename')
+User_parser.add_argument('surname')
+User_parser.add_argument('password')
+User_parser.add_argument('roles')
+
+
+class User(Resource):
+    def get(self, user_id):
+        user = UserModel.query.filter_by(id=user_id).first()
+
+        return jsonify({'User': {
+            'id': user.id,
+            'username': user.username,
+            'forename': user.forename,
+            'surname': user.surname,
+            'roles': user.roles
+        }})
+
+    def post(self):
+        data = User_parser.parse_args()
+        new_user = UserModel()
+        new_user.username = data['username']
+        new_user.forename = data['forename']
+        new_user.surname = data['surname']
+        new_user.password = data['password']
+        new_user.roles = [data['roles']]
+
+        try:
+            new_user.save_to_db()
+            return {
+                'message': 'User {} was created'.format(data['username'])
+            }
+        except:
+            return {'message': 'Something went wrong'}, 500
+
+
+api.add_resource(User, '/api/user', '/api/user/<int:user_id>')
