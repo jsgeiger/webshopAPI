4fce7d7cd29221ba1b7638573cba6419aa184b0a
Julia Geiger <juliageiger94@web.de>
2020-06-07 20:51:37 +0200

    refactor code

--- app/__init__.py
+++ app/__init__.py
@@ -7,11 +7,6 @@ from app.views import create_routes
 import app.config
 
 
-# @app.context_processor
-# def context_processor():
-#   return dict(user_manager=user_manager)
-
-
 def create_app():
     app = Flask(__name__)
     app.config.from_object(config.DevelopmentConfig)
@@ -19,14 +14,14 @@ def create_app():
     from app.model import db
     db.init_app(app)
 
-    from app.model import User, UserRoles, Author, Book, Role
+    from app.model.User import UserModel
+    from app.model.Author import AuthorModel
+    from app.model.Book import BookModel
     with app.app_context():
         db.create_all()
 
     CORS(app)
     api = Api(app)
     create_routes(api)
-
-    # user_manager = UserManager(app, db, User.UserModel)
 
     return app
